b4bdc797c49ac8f63e814707d5872bc8b5f62ca0
AdityaDipakPawar <adityadpawar71@gmail.com>
2025-12-12 15:02:16 +0530

    Grind-up

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -9,11 +9,12 @@ const Footer = () => {
           <img src="/Logos/logo.png" alt="Logo" />
         </div>
         <div className="footer-section">
-          <p>Know more about us :</p>
+          <span className="footer-label">Know more about us:</span>
           <div className="social-links">
-            <a href="#"><img src="/Logos/instagram-stroke-rounded.svg" alt="Instagram" /></a>
-            <a href="#"><img src="/Logos/email.svg" alt="Gmail" /></a>
-            <a href="#"><img src="/Logos/LinkedIn.svg" alt="LinkedIn" /></a>
+            <a href="https://www.instagram.com/grind._up?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><img src="/Logos/instagram.png" alt="Instagram" /></a>
+            <a href="mailto:07grindup@gmail.com" aria-label="Gmail"><img src="/Logos/gmail.svg" alt="Google" /></a>
+            <a href="https://www.linkedin.com/company/grindupco/" aria-label="LinkedIn"><img src="/Logos/LinkedIn Circled.svg" alt="LinkedIn" /></a>
+            <a href="#" aria-label="Twitter"><img src="/Logos/x.svg" alt="Twitter" /></a>
           </div>
         </div>
       </div>
@@ -24,13 +25,13 @@ const Footer = () => {
         <a href="#">About us</a>
         <a href="#">FAQ</a>
       </div>
-      <div className="footer-right">
+      {/* <div className="footer-right">
         <p>Download our app</p>
         <div className="app-buttons">
           <img src="/Logos/google-play.png" alt="Google Play" />
           <img src="/Logos/app-store.png" alt="App Store" />
         </div>
-      </div>
+      </div> */}
     </footer>
   )
 }
